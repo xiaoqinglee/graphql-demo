58661b79da4316e32f30f82cc0b0c8f522bd5d0a
lixiaoqing <xiaoqinglee@foxmail.com>
2023-03-06 17:03:28 +0800

    basic types

--- startup.sh
+++ startup.sh
@@ -10,13 +10,28 @@ npm init
 npm install graphql --save
 
 # https://graphql.org/graphql-js/
-node server.js
+node hello_world.js
 
 npm cache clean --force
 npm install express express-graphql graphql --save --legacy-peer-deps
 
 # https://graphql.org/graphql-js/running-an-express-graphql-server/
-node server.js
+node graphiql.js
 
 访问 http://localhost:4000/graphql
 打开浏览器 debugger, 查看底层的网络请求.
+
+
+# Basic Types
+# he GraphQL schema language supports the scalar types of String, Int, Float, Boolean, and ID,
+# so you can use these directly in the schema you pass to buildSchema.
+#
+#By default, every type is nullable - it's legitimate to return null as any of the scalar types.
+# Use an exclamation point to indicate a type cannot be nullable, so String! is a non-nullable string.
+#
+#To use a list type, surround the type in square brackets, so [Int] is a list of integers.
+#
+#Each of these types maps straightforwardly to JavaScript,
+# so you can just return plain old JavaScript objects in APIs that return these types.
+
+node basic_types.js
