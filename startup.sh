08fb376d1e64d033eacf12d40ff4d2d64b071f54
lixiaoqing <xiaoqinglee@foxmail.com>
2023-03-06 16:31:47 +0800

    GraphiQL

--- startup.sh
+++ startup.sh
@@ -11,3 +11,12 @@ npm install graphql --save
 
 # https://graphql.org/graphql-js/
 node server.js
+
+npm cache clean --force
+npm install express express-graphql graphql --save --legacy-peer-deps
+
+# https://graphql.org/graphql-js/running-an-express-graphql-server/
+node server.js
+
+访问 http://localhost:4000/graphql
+打开浏览器 debugger, 查看底层的网络请求.
